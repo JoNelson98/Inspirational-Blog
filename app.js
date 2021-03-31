3f199bdbf8df27ad0596c36a6c059d397a497774
Jonathan Nelson <59234939+JoNelson98@users.noreply.github.com>
2021-03-31 15:30:26 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//jonathan nelson
+window.onload = function() {
+    document.getElementById("my_audio").play();
+};
+//guess this doesnt work?
+
+setTimeout(function(){
+    document.getElementById("my_audio").play();
+}, 8000)
